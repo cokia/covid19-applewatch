09f819fca91297340896fec389051ea49f29628f
cokia <hwymaster01@gmail.com>
2020-02-25 11:42:37 +0900

    Initial Commit

---
+++ covid19-applewatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  covid19-applewatch WatchKit Extension
+//
+//  Created by h4nuko0n on 2020/02/25.
+//  Copyright © 2020 hanukoon. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
